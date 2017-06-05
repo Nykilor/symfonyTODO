0f30fcf2896a725b21c5638f4268f9e84620e1f1
Marmolada <smigi1997@gmail.com>
2017-06-06 00:59:37 +0200

    SymfonyTODO

--- src/AppBundle/Controller/CRUDController.php
+++ src/AppBundle/Controller/CRUDController.php
@@ -7,11 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
-/**
- * Crud controller.
- *
- * @Route("posts")
- */
 class CRUDController extends Controller
 {
     /**
